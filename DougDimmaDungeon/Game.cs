ec048d515af6085c74cdd35f070af2f7a6efee07
Yanchao Peng <yanchao.peng@mail.utoronto.ca>
2018-06-08 01:30:30 -0400

    added sections

--- DougDimmaDungeon/Game.cs
+++ DougDimmaDungeon/Game.cs
@@ -11,14 +11,35 @@ namespace DougDimmaDungeon
     {
         private static readonly int _screenWidth = 100;
         private static readonly int _screenHeight = 70;
-
         private static RLRootConsole _rootConsole;
 
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 48;
+        private static RLConsole _mapConsole;
+
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
+
         static void Main(string[] args)
-        {
+        {         
             string fontFileName = "terminal8x8.png";
             string consoleTitle = "DougDimmaDungeon - Level 1";
             _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, consoleTitle);
+
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
             _rootConsole.Update += OnRootConsoleUpdate;
             _rootConsole.Render += OnRootConsoleRender;
             _rootConsole.Run();
@@ -26,10 +47,25 @@ namespace DougDimmaDungeon
 
         private static void OnRootConsoleUpdate (Object sender, UpdateEventArgs e)
         {
-            _rootConsole.Print(10, 10, "Hallelujah", RLColor.LightRed);
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, RLColor.Black);
+            _mapConsole.Print(1, 1, "MAP", RLColor.White);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
+            _messageConsole.Print(1, 1, "MESSAGES", RLColor.White);
+
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, RLColor.Brown);
+            _statConsole.Print(1, 1, "STATS", RLColor.White);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
+            _inventoryConsole.Print(1, 1, "INVENTORY", RLColor.White);
         }
         private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
         {
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight, _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight, _rootConsole, 0, 0);
+
             _rootConsole.Draw();
         }
     }
