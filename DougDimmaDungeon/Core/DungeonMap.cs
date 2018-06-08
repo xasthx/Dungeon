f560e0cb38e09a8e12f7c25254e4b1cd0c1b4e1b
Yanchao Peng <yanchao.peng@mail.utoronto.ca>
2018-06-08 16:57:28 -0400

    added framework for dungeon

---
+++ DougDimmaDungeon/Core/DungeonMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RLNET;
+using RogueSharp;
+
+namespace DougDimmaDungeon.Core
+{
+    public class DungeonMap : Map
+    {
+        public void Draw(RLConsole mapConsole)
+        {
+            mapConsole.Clear();
+            foreach(Cell cell in GetAllCells())
+            {
+                SetConsoleSymbolForCell(mapConsole, cell);
+            }
+        }
+
+        private void SetConsoleSymbolForCell(RLConsole console, Cell cell)
+        {
+            if (!cell.IsExplored)
+            {
+                return;
+            }
+            if (IsInFov(cell.X, cell.Y))
+            {
+                // '.' is floor, '#' is wall
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Colors.FloorFov, Colors.FloorBackgroundFov, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, ColorsWallFov, Colors.WallBackgroundFov, '#');
+                }
+            }
+            else
+            {
+                if (cell.IsWalkable)
+                {
+                    console.Set(cell.X, cell.Y, Colors.Floor, Colors.FloorBackground, '.');
+                }
+                else
+                {
+                    console.Set(cell.X, cell.Y, Colors.Wall, Colors.WallBackground, '#');
+                }
+            }
+        }
+    }
+}
